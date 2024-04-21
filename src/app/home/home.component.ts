8a7939accb7abe8f06f2ce2989edd776565adb33
dudd1ts <kserg@gmail.com>
2024-04-21 16:09:56 +0300

    feat: made a product component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import { Products } from '../../types';
+import { Products, Product } from '../../types';
 import { ProductsService } from '../services/products.service';
+import { ProductComponent } from '../components/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
+
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product)
+  }
   
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
